b62cd3b27e72b5491e504b12fe93dd9dbe61fedf
Henry Canapp <henrycanapp@ufl.edu>
2025-04-16 18:10:29 -0400

    Debugged some functions Fixed file formatting (maybe?)

--- Week12Lab_A.py
+++ Week12Lab_A.py
@@ -1,15 +1,16 @@
+import Week12Lab_C
 import console_gfx
-import Week12Lab_B
 
 print("Welcome to the RLE image encoder!")
 print("\nDisplaying Spectrum Image:")
 console_gfx.display_image(console_gfx.test_rainbow)
+print()
 file_data = []
 while True:
-    print("\n\nRLE Menu\n--------\n0. Exit\n1. Load File\n2. Load Test Image")
+    print("\nRLE Menu\n--------\n0. Exit\n1. Load File\n2. Load Test Image")
     print("3. Read RLE String\n4. Read RLE Hex String\n5. Read Data Hex String")
     print("6. Display Image\n7. Display RLE String\n8. Display Hex RLE Data\n9. Display Hex Flat Data")
-    option = int(input("\nSelect a Menu Option:"))
+    option = int(input("\nSelect a Menu Option: "))
     if option == 0:
         break
     elif option == 1:
@@ -17,7 +18,33 @@ while True:
         file_data = console_gfx.load_file(fileName)
     elif option == 2:
         file_data = console_gfx.test_image
+        print("Test image data loaded.")
+    elif option == 3:
+        rle_string = str(input("Enter an RLE string to be decoded: "))
+        file_data = Week12Lab_C.decode_rle(Week12Lab_C.string_to_rle(rle_string))
+    elif option == 4:
+        hex_rle_string = str(input("Enter the hex string holding RLE data: "))
+        file_data = Week12Lab_C.decode_rle(Week12Lab_C.string_to_data(hex_rle_string))
+    elif option == 5:
+        hex_string = str(input("Enter the hex string holding flat data: "))
+        file_data = Week12Lab_C.string_to_data(hex_string)
     elif option == 6:
-        console_gfx.display_image(file_data)
+        print("Displaying image...")
+        print("(no data)") if len(file_data) == 0 else console_gfx.display_image(file_data)
     elif option == 7:
-        print(Week12Lab_B.get_decoded_length(Week12Lab_B.encode_rle(file_data)))
+        if len(file_data) == 0:
+            print(f"RLE representation: (no data)")
+        else:
+            print(f"RLE representation: {Week12Lab_C.to_rle_string(Week12Lab_C.encode_rle(file_data))}")
+    elif option == 8:
+        if len(file_data) == 0:
+            print(f"RLE hex values: (no data)")
+        else:
+            print(f"RLE hex values: {Week12Lab_C.to_hex_string(Week12Lab_C.encode_rle(file_data))}")
+    elif option == 9:
+        if len(file_data) == 0:
+            print(f"Flat hex values: (no data)")
+        else:
+            print(f"Flat hex values: {Week12Lab_C.to_hex_string(file_data)}")
+    else:
+        print("Error! Invalid input.")
--- Week12Lab_C.py
+++ Week12Lab_C.py
@@ -1,4 +1,4 @@
-import console_gfx
+
 
 #translates data to hexadecimal string
 def to_hex_string(data):
@@ -65,53 +65,12 @@ def to_rle_string(rle_data):
 
 #turns a human-readable string into RLE data (array)
 def string_to_rle(rle_string : str):
-    data_string = rle_string.replace(":", "")
-    return string_to_data(data_string)
-
-print("Welcome to the RLE image encoder!")
-print("\nDisplaying Spectrum Image:")
-console_gfx.display_image(console_gfx.test_rainbow)
-print()
-file_data = []
-while True:
-    print("\nRLE Menu\n--------\n0. Exit\n1. Load File\n2. Load Test Image")
-    print("3. Read RLE String\n4. Read RLE Hex String\n5. Read Data Hex String")
-    print("6. Display Image\n7. Display RLE String\n8. Display Hex RLE Data\n9. Display Hex Flat Data")
-    option = int(input("\nSelect a Menu Option: "))
-    if option == 0:
-        break
-    elif option == 1:
-        fileName = str(input("Enter name of file to load: "))
-        file_data = console_gfx.load_file(fileName)
-    elif option == 2:
-        file_data = console_gfx.test_image
-        print("Test image data loaded.")
-    elif option == 3:
-        rle_string = str(input("Enter an RLE string to be decoded: "))
-        file_data = decode_rle(string_to_rle(rle_string))
-    elif option == 4:
-        hex_rle_string = str(input("Enter the hex string holding RLE data: "))
-        file_data = decode_rle(string_to_data(hex_rle_string))
-    elif option == 5:
-        hex_string = str(input("Enter the hex string holding flat data: "))
-        file_data = string_to_data(hex_string)
-    elif option == 6:
-        print("Displaying image...")
-        print("(no data)") if len(file_data) == 0 else console_gfx.display_image(file_data)
-    elif option == 7:
-        if len(file_data) == 0:
-            print(f"RLE representation: (no data)")
+    hex_string = ""
+    for i in rle_string.split(":"):
+        if len(i) > 2:
+            hex_string += '%x' % int(i[:2])
+            hex_string += i[-1]
         else:
-            print(f"RLE representation: {to_rle_string(encode_rle(file_data))}")
-    elif option == 8:
-        if len(file_data) == 0:
-            print(f"RLE hex values: (no data)")
-        else:
-            print(f"RLE hex values: {to_hex_string(encode_rle(file_data))}")
-    elif option == 9:
-        if len(file_data) == 0:
-            print(f"Flat hex values: (no data)")
-        else:
-            print(f"Flat hex valuess: {to_hex_string(file_data)}")
-    else:
-        print("Error! Invalid input.")
+            hex_string += '%x' % int(i[0])
+            hex_string += '%x' % int(i[1])
+    return string_to_data(hex_string)
